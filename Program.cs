cb82dfb563d3967f66e6439d7e66f43ef0e6bc66
Nicholas Davidson <nicholas.davidson.20@gmail.com>
2024-04-22 21:32:57 -0500

    changes

--- Program.cs
+++ Program.cs
@@ -1,3 +1,4 @@
+using HackVisualVirtuosoBE.Models;
 using Microsoft.EntityFrameworkCore;
 using System.Text.Json.Serialization;
 using Microsoft.AspNetCore.Http.Json;
@@ -8,6 +9,18 @@ var builder = WebApplication.CreateBuilder(args);
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+// allows passing datetimes without time zone data 
+AppContext.SetSwitch("Npgsql.EnableLegacyTimestampBehavior", true);
+
+// allows our api endpoints to access the database through Entity Framework Core
+builder.Services.AddNpgsql<HackVisualVirtuosoBEDbContext>(builder.Configuration["HackVisualVirtuosoBEDbConnectionString"]);
+
+// Set the JSON serializer options
+builder.Services.Configure<JsonOptions>(options =>
+{
+    options.SerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles;
+});
 
 var app = builder.Build();
 
