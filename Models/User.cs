0f3bbc135e7be7f9f870d9c9c7669816ba5d5e2f
Luca Bonini <luca14bonini@gmail.com>
2024-04-22 19:57:35 -0700

    Setup done

---
+++ Models/User.cs
@@ -0,0 +1,11 @@
+namespace HackVisualVirtuosoBE.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public string Bio { get; set; }
+        public string? Email { get; set; }
+        public int Uid { get; set; }
+    }
+}
