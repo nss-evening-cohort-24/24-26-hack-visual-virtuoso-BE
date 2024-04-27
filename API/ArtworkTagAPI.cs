b3646f48abf129138be030126c83df260a57b0b0
Luca Bonini <luca14bonini@gmail.com>
2024-04-27 10:07:09 -0700

    Changed call for delete tag from artwork

--- API/ArtworkTagAPI.cs
+++ API/ArtworkTagAPI.cs
@@ -26,7 +26,7 @@ namespace HackVisualVirtuosoBE.API
                 return Results.Ok();
             });
 
-            app.MapPost("/artwork/removetag", async (HackVisualVirtuosoBEDbContext _context, AddTagToArtworkDto addTagToArtworkDto) =>
+            app.MapDelete("/artwork/removetag", async (HackVisualVirtuosoBEDbContext _context, AddTagToArtworkDto addTagToArtworkDto) =>
             {
                 Artwork artwork = await _context.Artwork.FirstOrDefaultAsync(t => t.Id == addTagToArtworkDto.ArtworkId);
                 Tag tag = await _context.Tags.FirstOrDefaultAsync(t => t.Id == addTagToArtworkDto.TagId);
