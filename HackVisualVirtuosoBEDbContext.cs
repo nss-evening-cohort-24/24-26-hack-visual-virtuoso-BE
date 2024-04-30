8c871952052d1445d9ccac170daf39fe406cd225
Luca Bonini <luca14bonini@gmail.com>
2024-04-29 18:53:38 -0700

    Mock data added to the DB file for Tags and artworkTags

--- HackVisualVirtuosoBEDbContext.cs
+++ HackVisualVirtuosoBEDbContext.cs
@@ -22,5 +22,24 @@ public class HackVisualVirtuosoBEDbContext : DbContext
             new Artwork { Id = 2, Title = "Mona Lisa", ImageUrl = "https://t1.gstatic.com/licensed-image?q=tbn:ANd9GcQsu7yYuRPXNK9eHHSFD2tUYO4stQDb1Ez8vjqGERfs9xqYLLnY_y6lQkPFZa-44cqn", Description = "", UserId = "2"},
             new Artwork { Id = 3, Title = "The Starry Night", ImageUrl = "https://lh3.googleusercontent.com/Pd2nCUHUz4Ruc76LRh1-H0Dldl04hWSXw8P9uCYZ4TIWP7yNPArIgWlHZrf1qT9T=s1200", Description = "", UserId = "3"}
         });
+
+        modelBuilder.Entity<ArtworkTag>().HasData(new ArtworkTag[]
+        {
+            new ArtworkTag { Id = 1, ArtworkId = 1, TagId = 1 },
+        });
+
+        modelBuilder.Entity<Tag>().HasData(new Tag[]
+        {
+            new Tag { Id = 1, Name = "Surrealism" },
+            new Tag { Id = 2, Name = "Impressionism"},
+            new Tag { Id = 3, Name = "Abstract"},
+            new Tag { Id = 4, Name = "Realism"},
+            new Tag { Id = 5, Name = "Cubism"},
+            new Tag { Id = 6, Name = "Portraiture"},
+            new Tag { Id = 7, Name = "Romanticism"}
+        });
     }
+
+
 }
+   
