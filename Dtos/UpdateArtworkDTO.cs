337ba6fb901863bb5bffea449f9452699cae488c
Luca Bonini <luca14bonini@gmail.com>
2024-05-07 20:55:18 -0700

    Fix to update Artwork

--- Dtos/UpdateArtworkDTO.cs
+++ Dtos/UpdateArtworkDTO.cs
@@ -5,5 +5,6 @@ namespace HackVisualVirtuosoBE.Dtos
         public string Title { get; set; }
         public string Description { get; set; }
         public string ImageUrl { get; set;}
+        public List<int> TagIds { get; set; }
     }
 }
